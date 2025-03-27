d646e630b68367385c0a88e1459a3bcceabc522b
ANURAG PRAJAPATI <anuragprajapati02005@gmail.com>
2025-03-27 07:19:36 +0530

    initial commit

--- ExtendedEuclidean.c
+++ ExtendedEuclidean.c
@@ -1,22 +1,36 @@
-#include<stdio.h>
-long long gcd(long long a,long long b){
-    while(b!=0){
-        long long r=a%b;
-        a=b;
-        b=r;
-    }
-    return a;
+// C program to demonstrate working of 
+// extended Euclidean Algorithm 
+#include <stdio.h>
+
+// Function for extended Euclidean Algorithm 
+int gcdExtended(int a, int b, int *x, int *y) {
+
+    // Base Case 
+    if (a == 0) { 
+        *x = 0; 
+        *y = 1; 
+        return b; 
+    } 
+
+    int x1, y1; 
+    int gcd = gcdExtended(b % a, a, &x1, &y1); 
+
+    // Update x and y using results of 
+    // recursive call 
+    *x = y1 - (b / a) * x1; 
+    *y = x1; 
+    return gcd; 
+} 
+
+int findGCD(int a, int b) {
+
+    int x = 1, y = 1;
+    return gcdExtended(a, b, &x, &y);
 }
 
-int main(){
-    long long a,b;
-    scanf("%lld %lld",&a,&b);
-    if(b>a){
-        long long temp=b;
-        b=a;
-        a=temp;
-    }
-    long long r=gcd(a,b);
-    printf("%lld",r);
-    return 0;
+int main() { 
+    int a = 35, b = 15;
+    int g = findGCD(a, b); 
+    printf("%d\n", g);
+    return 0; 
 }
